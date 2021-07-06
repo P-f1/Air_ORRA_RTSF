02c29e12b0243a294c2d12ddc252be8a00d44170
SteveNY-Tibco <chuyang@tibco.com>
2021-07-06 18:22:51 -0400

    update build script

--- build/build.sh
+++ build/build.sh
@@ -10,6 +10,9 @@ echo "BuildFolder = "$BuildFolder
 echo "EXT_HOME_DIR = " $EXT_HOME_DIR
 echo "FRAMEWORK = " $FRAMEWORK
 
+rm -rf ../artifacts/rtsf-at-checkout-reference-design
+git clone https://github.com/intel-iot-devkit/rtsf-at-checkout-reference-design.git ../artifacts/rtsf-at-checkout-reference-design
+
 cp -r ../artifacts/rtsf-at-checkout-reference-design/rtsf-at-checkout-cv-region-of-interest ./
 cd ./rtsf-at-checkout-cv-region-of-interest
 docker build -t cv-region-of-interest:dev ./
